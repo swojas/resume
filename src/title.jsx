bddf2c7ea302145448522d45ae9dc656b9ee477c
Swojas Bhalunkar <swojas12@gmail.com>
2023-07-28 14:18:56 +0530

    Update title.jsx

--- src/title.jsx
+++ src/title.jsx
@@ -6,7 +6,7 @@ function AboutMe() {
       <div className="profile-info">
         <img className="profile-img" src="https://avatars.githubusercontent.com/u/60619308?v=4" alt="Profile Picture" />
         <div className="profile-details">
-          <h2>John Doe</h2>
+          <h2>Swojas Bhalunkar</h2>
           <p>Email: swojas12@gmail.com</p>
           <p>Phone: (+91) 9130313369</p>
         </div>
